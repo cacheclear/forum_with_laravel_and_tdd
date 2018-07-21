726e0b228695e5c752b739b5379875ddfad0e581
cacheclear <contact@dennisking.de>
2018-07-21 14:10:26 +0200

    splits tests for thread into several methods

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -11,13 +11,19 @@ class ThreadsTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function a_user_can_browse_threads()
+    public function a_user_can_browse_all_threads()
     {
         $thread = factory(Thread::class)->create();
         $response = $this->get('/threads');
 
         $response->assertSee($thread->title);
+    }
 
+    /** @test */
+    public function a_user_can_browse_a_single_thread()
+    {
+        $thread = factory(Thread::class)->create();
+        
         $response = $this->get('/threads/' . $thread->id);
         $response->assertSee($thread->title);
     }
