2fd4b3f24af6b3bf12f630b071c6aa0a89dc339d
cacheclear <contact@dennisking.de>
2018-07-23 09:33:52 +0200

    adds test for thread owner

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Thread;
+use App\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -13,14 +14,31 @@ class ThreadTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public $thread;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        //Arrange
+        $this->thread = factory(Thread::class)->create();
+    }
+
     /** @test */
     public function a_thread_has_replies()
     {
-        //Arrange
-        $thread = factory(Thread::class)->create();
         //Act
-        $replies = $thread->replies;
+        $replies = $this->thread->replies;
         //Assert
         $this->assertInstanceOf(Collection::class, $replies);
+    }
+
+    /** @test */
+    public function a_thread_has_a_creator()
+    {
+        //Act
+        $owner = $this->thread->owner;
+        //Assert
+        $this->assertInstanceOf(User::class, $owner);
     }
 }
